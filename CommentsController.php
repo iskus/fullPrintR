20730dec1861cd420a7feaf8586af6f5418c5484
Iskus <iskus1981@yandex.ru>
2015-01-04 11:59:31 +0200

    Update CommentsController.php

--- CommentsController.php
+++ CommentsController.php
@@ -1,22 +1,10 @@
 <?php
 
-class CommentsController extends BaseController {
+class FullPrintR{
 
 	public $sp = '';
 	public $i = 0;
 	public $out = '';
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('comments', 'CommentsController@getIndex');
-	|
-	*/
 
 	public function getIndex()
 	{
